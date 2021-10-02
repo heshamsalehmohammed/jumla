1c6f2a84017f4d3016450d2568f8549ef3a14365
hesham.saleh.mohammed <hesham.saleh.mohammed@gmail.com>
2021-10-02 06:10:23 +0200

    sections

--- src/components/pages/LandingPage/LandingPage.jsx
+++ src/components/pages/LandingPage/LandingPage.jsx
@@ -4,14 +4,20 @@ import NavBar from '../../common/navBar/navBar';
 import Footer from '../../common/footer/footer';
 import MainBanner from './Sections/MainBanner/MainBanner';
 import AboutUs from './Sections/AboutUs/AboutUs';
+import ContactUs from './Sections/ContactUs/ContactUs';
+import Services from './Sections/Services/Services';
 import LanguageSelector from '../../common/LanguageSelector';
+import FancyAnimation from './Sections/FancyAnimation/FancyAnimation';
 
 const LandingPage = (props) => {
   return (
     <>
       <NavBar />
       <MainBanner />
+{/*       <FancyAnimation/> */}
       <AboutUs />
+      <Services/>
+      <ContactUs/>
       <Footer />
       {/*  <LanguageSelector/> */}
     </>
